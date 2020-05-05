1267adcd8971fd50df3300decb4b592133c266d0
Summer <2641541955@qq.com>
2020-05-05 20:14:51 +0800

    第一次上传github

---
+++ src/main/java/com/cn/Test1.java
@@ -0,0 +1,14 @@
+package com.cn;
+
+/**
+ * @author Dark
+ * @date 2020-04-24 8:14
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Test1");
+    }
+    public void test1(){
+        System.out.println("test1,改代码");
+    }
+}
